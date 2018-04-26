324680b11955af90a06830d4bf2dd2ccf85c2e53
Doljko <doljko_tv@yahoo.com>
2018-04-26 18:52:45 +0900

    Created Onclick in MainActivity.

--- app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.doljko.finger_walking_snowy_scene;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button myFirstButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myFirstButton = (Button) findViewById(R.id.b1);
+        myFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"I was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
