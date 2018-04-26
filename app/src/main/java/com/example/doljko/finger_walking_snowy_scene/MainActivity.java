3660b3a46671e9e4116bfc92b73b0ee653229b26
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-04-26 22:21:03 +0800

    Touch listener bug fixed.

--- app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
@@ -1,51 +1,46 @@
 package com.example.doljko.finger_walking_snowy_scene;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout myLayout = null;
+    private RelativeLayout myLayout;
 
-    private Button luckyID= null;
+    private ImageButton luckyID;
 
     private float x;
     private float y;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
        myLayout = (RelativeLayout)findViewById(R.id.myLayout);
-       luckyID = (Button)findViewById(R.id.luckyID);
-
-
-
+       luckyID = (ImageButton)findViewById(R.id.luckyID);
 
        myLayout.setOnTouchListener(new View.OnTouchListener() {
 
-           int i = 0;
            @Override
            public boolean onTouch(View v, MotionEvent event) {
 
                x = event.getX();
-               y = event.getX();
+               y = event.getY();
                if (event.getAction() == MotionEvent.ACTION_MOVE) {
 
                    luckyID.setX(x); //set the coordinate
-                   luckyID.setX(y);
+                   luckyID.setY(y);
                }
-               if (++i >50){
 
-                   return false;
-
-               }
                 return true;
            }
 
