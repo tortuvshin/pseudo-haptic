9b204bf5b22039845028e632fd0e4e57d15987f7
Doljko <doljko_tv@yahoo.com>
2018-04-26 19:19:30 +0900

    Created OnTouch in MainActivity.

--- app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
@@ -2,27 +2,54 @@ package com.example.doljko.finger_walking_snowy_scene;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button myFirstButton = null;
+    private RelativeLayout myLayout = null;
+
+    private Button luckyID= null;
+
+    private float x;
+    private float y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myFirstButton = (Button) findViewById(R.id.b1);
-        myFirstButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"I was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+       myLayout = (RelativeLayout)findViewById(R.id.myLayout);
+       luckyID = (Button)findViewById(R.id.luckyID);
 
+
+
+
+       myLayout.setOnTouchListener(new View.OnTouchListener() {
+
+           int i = 0;
+           @Override
+           public boolean onTouch(View v, MotionEvent event) {
+
+               x = event.getX();
+               y = event.getX();
+               if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+                   luckyID.setX(x); //set the coordinate
+                   luckyID.setX(y);
+               }
+               if (++i >50){
+
+                   return false;
+
+               }
+                return true;
+           }
+
+       });
 
     }
 }
