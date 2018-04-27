f0540309cfe017b8cf3ad59bddd57a76aacbac72
Doljko <doljko_tv@yahoo.com>
2018-04-27 17:18:31 +0900

    Sound button Created

---
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/SoundActivity.java
@@ -0,0 +1,15 @@
+package com.example.doljko.finger_walking_snowy_scene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound);
+    }
+
+}
---
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/SoundDemoActivity.java
@@ -0,0 +1,13 @@
+package com.example.doljko.finger_walking_snowy_scene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_demo);
+    }
+}
