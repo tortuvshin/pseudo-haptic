1ad88914cf68ecd4ac7b3c13959f099a8f70d5e1
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-05-09 17:58:06 +0900

    Create on swipe touchListener

--- app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/com/example/doljko/finger_walking_snowy_scene/MainActivity.java
@@ -6,47 +6,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout myLayout;
+public class MainActivity extends AppCompatActivity{
 
-    private ImageButton luckyID;
+    private RelativeLayout myView;
+    private ImageButton footStep;
 
     private float x;
     private float y;
-
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myView = (RelativeLayout)findViewById(R.id.myView);
+        footStep = (ImageButton)findViewById(R.id.footStep);
 
-       myLayout = (RelativeLayout)findViewById(R.id.myLayout);
-       luckyID = (ImageButton)findViewById(R.id.luckyID);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.snow);
+        myView.setOnTouchListener(new OnSwipeTouchListener(this) {
 
-       final MediaPlayer mp = MediaPlayer.create(this, R.raw.snow);
-       myLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mp.start();
+                x = event.getX();
+                y = event.getY();
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
 
-           @Override
-           public boolean onTouch(View v, MotionEvent event) {
-               mp.start();
-               x = event.getX();
-               y = event.getY();
-               if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    footStep.setX(x); //set the coordinate
+                    footStep.setY(y);
+                }
 
-                   luckyID.setX(x); //set the coordinate
-                   luckyID.setY(y);
-               }
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+
+                    Toast.makeText(MainActivity.this, "Swipe down"+event.getAction(), Toast.LENGTH_SHORT).show();
+                }
 
                 return true;
-           }
+            }
 
-       });
+            @Override
+            public void onSwipeDown() {
+                super.onSwipeDown();
 
+                Toast.makeText(MainActivity.this, "Swipe down", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
     }
+
 }
+
