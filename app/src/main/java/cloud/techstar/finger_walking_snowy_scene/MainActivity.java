fb2ff1464808ba67874db2766749e2d2d0c18910
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-05-09 22:51:53 +0900

    textview set values

--- app/src/main/java/cloud/techstar/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/cloud/techstar/finger_walking_snowy_scene/MainActivity.java
@@ -1,61 +1,61 @@
 package cloud.techstar.finger_walking_snowy_scene;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
     private ImageView snowBackground;
-    private ImageButton footStep;
 
     private float y;
     private float dy;
+    private float x;
+    private float dx;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        snowBackground = (ImageView)findViewById(R.id.snow_background);
-        footStep = (ImageButton)findViewById(R.id.footStep);
-
-
-//        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
-//        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
-//        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
-//        animator.setRepeatCount(ValueAnimator.INFINITE);
-//        animator.setInterpolator(new LinearInterpolator());
-//        animator.setDuration(10000L);
-//        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                final float progress = (float) animation.getAnimatedValue();
-//                final float width = backgroundOne.getWidth();
-//                final float translationX = width * progress;
-//                backgroundOne.setTranslationX(translationX);
-//                backgroundTwo.setTranslationX(translationX - width);
-//            }
-//        });
-//        animator.start();
+        snowBackground = (ImageView) findViewById(R.id.snow_background);
+        final TextView valueText = (TextView) findViewById(R.id.y_value);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.snow);
         snowBackground.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                mp.start();
+//                mp.start();
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN: {
                         y = event.getY();
                         dy = y - snowBackground.getY();
+                        x = event.getX();
+                        dx = x - snowBackground.getX();
+
                     }
                     break;
                     case MotionEvent.ACTION_MOVE: {
                         snowBackground.setY(event.getY() - dy);
+//                        snowBackground.setX(event.getX() - dx);
+
+                        valueText.setText("BEFORE VALUE:  Y "+y +" X "+x +
+                                "\nCURRENT VALUE: Y "+ event.getY()+ " X "+event.getX()+
+                                "\nCHANGED VALUE: Y "+ (event.getY()-dy) + " X "+(event.getX() - dx));
                     }
                     break;
                     case MotionEvent.ACTION_UP: {
@@ -68,6 +68,5 @@ public class MainActivity extends AppCompatActivity{
 
         });
     }
-
 }
 
