2a01319570b83289e7161405a1d6d9bc07833c54
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-05-16 17:45:38 +0900

    тайлбарууд бичсэн

--- app/src/main/java/cloud/techstar/finger_walking_snowy_scene/MainActivity.java
+++ app/src/main/java/cloud/techstar/finger_walking_snowy_scene/MainActivity.java
@@ -1,31 +1,29 @@
 package cloud.techstar.finger_walking_snowy_scene;
 
-import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.animation.LinearInterpolator;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
-    private ImageView snowBackground;
+    private ImageView snowBackground; // Цасны гадаргуу харуулж байгаа ImageView
 
-    private float y;
-    private float dy;
-    private float x;
-    private float dx;
+    /**
+     * Цасан дээр алхаж байгаа мэт мэдрэмжийг төрүүлэхийн тулд хэрэглэгч дэлгэц дээр
+     * ямар хэсэгт хүрсэн /X, Y координатуудын хувьд/, тэнхлэгийн утга нь байрлал болон бусад
+     * хөдөлгөөний шинж чанарын хувьд хэрхэн өөрчлөгдсөн бэ гэдгийг мэдэх хэрэгтэй болно.
+     * Ингэхийн тулд дараах X, Y тэнхлэгийн утга эхний удаа дэлгэцэнд хүрхэд ямар байсан
+     * дараа нь хэрхэн өөрчлөгдсөн бэ гэдгийг мэдэх хэрэгтэй болно
+     * */
+    private float y; // Y тэнхлэгт эхлээд хаана хүрсэн
+    private float dy; // Y тэнхлэгт дараа нь хаана хүрч утга нь өөрчлөгдсөн
+    private float x; // X тэнхлэгт эхлээд хаана хүрсэн
+    private float dx; // X тэнхлэгт дараа нь хаана хүрч утга нь өөрчлөгдсөн
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -35,15 +33,32 @@ public class MainActivity extends AppCompatActivity{
         snowBackground = (ImageView) findViewById(R.id.snow_background);
         final TextView valueText = (TextView) findViewById(R.id.y_value);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.snow);
+
+        // Background дээр хүрэх event ийг барих
         snowBackground.setOnTouchListener(new View.OnTouchListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 //                mp.start();
+
+                /** Хэрэглэгч дэлгэцэн дээр хүрэх үед систем холбогдох эвэнтүүдийг шалгах
+                 * Доошоо, дээшээ, баруун, зүүн гэх мэт олон төрлийн хөдөлгөөнүүдийг MotionEvent-ийн
+                 * тусламжтайгаар барьж авна
+                 * {@link https://developer.android.com/reference/android/view/MotionEvent}.
+                 */
                 switch (event.getAction()) {
+
+                    /**
+                     * y, dy, x, dy утгууд ямар учиртай вэ гэвэл
+                     * Алхах хөдөлгөөн хийхийн тулд эхний хөл гишгээд дараагийн хөл урд гишгэдэг
+                     * y,x утга дээр эхлээд хаана гишгэсэн
+                     * dy, dx утга дээр дараа нь хаана гишгэсэн гэдгийг авч байна гэж ойлгох хэрэгтэй
+                     * Арын цасыг хэрхэн хөдөлгөх вэ гэвэл эхний гишгэсэн утгаас дараагийн утгыг хасаад ялгаварын хэмжээгээр
+                     * арын зургаа Y тэнхлэгийн дагуу хөдөлгөнө гэсэн үг
+                     * */
                     case MotionEvent.ACTION_DOWN: {
-                        y = event.getY();
-                        dy = y - snowBackground.getY();
+                        y = event.getY(); // А
+                        dy = y - snowBackground.getY(); //
                         x = event.getX();
                         dx = x - snowBackground.getX();
 
@@ -51,6 +66,7 @@ public class MainActivity extends AppCompatActivity{
                     break;
                     case MotionEvent.ACTION_MOVE: {
                         snowBackground.setY(event.getY() - dy);
+
 //                        snowBackground.setX(event.getX() - dx);
 
                         valueText.setText("BEFORE VALUE:  Y "+y +" X "+x +
