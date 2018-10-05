709a94972d5a7e73d25ce19225e5023cf0b2246c
Doljko <doljko927@gmail.com>
2018-10-05 14:59:03 +0900

    SnowFlate layout added.

--- app/src/main/java/cloud/techstar/finger_walking_snowy_scene/SnowFall.java
+++ app/src/main/java/cloud/techstar/finger_walking_snowy_scene/SnowFall.java
@@ -9,5 +9,17 @@ public class SnowFall extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snow_fall);
+        SnowFlakesLayout snowFlakesLayout = (SnowFlakesLayout)findViewById(R.id.snow_flake);
+        snowFlakesLayout.init();
+        snowFlakesLayout.setWholeAnimateTiming(3000000);
+        snowFlakesLayout.setAnimateDuration(5000);
+        snowFlakesLayout.setGenerateSnowTiming(50);
+        snowFlakesLayout.setRandomSnowSizeRange(40, 1); // snow size
+        snowFlakesLayout.setImageResourceID(R.drawable.snow_flakes_pic);
+        snowFlakesLayout.setEnableRandomCurving(true);
+        snowFlakesLayout.setEnableAlphaFade(true);
+        snowFlakesLayout.startSnowing();
     }
+
 }
+
