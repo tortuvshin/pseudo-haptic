170c4e7819bfc296145276799a1ab5fa0c5c9fff
Doljko <doljko927@gmail.com>
2018-10-05 14:37:19 +0900

    SnowFlall Layout added.

---
+++ app/src/main/java/cloud/techstar/finger_walking_snowy_scene/SnowFall.java
@@ -0,0 +1,13 @@
+package cloud.techstar.finger_walking_snowy_scene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnowFall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snow_fall);
+    }
+}
