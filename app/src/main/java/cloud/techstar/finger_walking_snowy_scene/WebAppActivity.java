727bc2d1ac328ba8792e5e23586b6b3571871c48
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-11-15 00:58:14 +0900

    мөр нэмсэн

--- app/src/main/java/cloud/techstar/finger_walking_snowy_scene/WebAppActivity.java
+++ app/src/main/java/cloud/techstar/finger_walking_snowy_scene/WebAppActivity.java
@@ -1,11 +1,22 @@
 package cloud.techstar.finger_walking_snowy_scene;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Vibrator;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,7 +24,10 @@ import java.io.InputStreamReader;
 
 public class WebAppActivity extends AppCompatActivity {
 
-    @SuppressLint("SetJavaScriptEnabled")
+    boolean touchCounter = false;
+    private Vibrator vibrator;
+    private MediaPlayer mp;
+    @SuppressLint({"SetJavaScriptEnabled", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +36,45 @@ public class WebAppActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        myWebView.loadUrl("file:android_asset/snow/viewer.html");
 
-        String folderPath = "file:android_asset/";
+        mp = MediaPlayer.create(this, R.raw.snow);
 
-        String fileName = "snow/viewer.html";
+        SnowFlakesLayout snowFlakesLayout = (SnowFlakesLayout)findViewById(R.id.web_snow_flake);
+        snowFlakesLayout.init();
+        snowFlakesLayout.setWholeAnimateTiming(3000000);
+        snowFlakesLayout.setAnimateDuration(5000);
+        snowFlakesLayout.setGenerateSnowTiming(50);
+        snowFlakesLayout.setRandomSnowSizeRange(40, 1); // snow size
+        snowFlakesLayout.setImageResourceID(R.drawable.snow_flakes_pic);
+        snowFlakesLayout.setEnableRandomCurving(true);
+        snowFlakesLayout.setEnableAlphaFade(true);
+        snowFlakesLayout.startSnowing();
+    }
 
-        String file = folderPath + fileName;
+    public class WebAppInterface {
+        Context mContext;
 
-        myWebView.loadUrl(file);
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void soundStart(){
+            mp.start();
+        }
+
+        @JavascriptInterface
+        public void vibrationStart(){
+            vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            assert vibrator != null;
+            vibrator.vibrate(50);
+        }
     }
 }
