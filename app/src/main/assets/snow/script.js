0356cebb9a988536e0f6529cdb85e0bedde64c3d
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-12-26 14:10:20 +0900

    updated

--- app/src/main/assets/snow/script.js
+++ app/src/main/assets/snow/script.js
@@ -3,34 +3,41 @@ scrollingElement = (document.scrollingElement || document.body);
 scrollingElement.scrollTop = scrollingElement.scrollHeight/2;
 scrollingElement.scrollLeft = scrollingElement.scrollWidth/2;
 
-//var startPageX;
-//var startPageY;
-//var startScreenX;
-//var startScreenY;
-//var movedDistance;
+var startPageX;
+var startPageY;
+var startScreenX;
+var startScreenY;
+var movedDistance;
 
 document.body.addEventListener('touchstart', function(e){
 //    var touchObj = e.changedTouches[0] // reference first touch point (ie: first finger)
 //    startScreenX = touchObj.clientX
 //    startScreenY = touchObj.clientY // get position of touch point relative to left edge of browser
       createFootPrint()
-//    e.preventDefault()
+    e.preventDefault()
 //
 //    startPageX = touchObj.pageX;
 //    startPageY = touchObj.pageY;
 //
 //    console.log("clientX coords: X: " + startScreenX + ", Y: " + startScreenX + " pageX coords: X: " + startPageX + ", Y: " + startPageY);
+    forEachChangedFinger(e, function(x,y, id) {
+        createDiv(finger, id);
+        moveBox(id, e2);
+    });
+})
 
-}, false)
 
-//
-//document.body.addEventListener('touchmove', function(e){
+document.body.addEventListener('touchmove', function(e){
 //    var touchObj = e.changedTouches[0] // reference first touch point for this event
 //    movedDistance = touchObj.clientY - startScreenY
-//    e.preventDefault()
-//}, false)
-//
-//document.body.addEventListener('touchend', function(e){
+    e.preventDefault(); // prevent page scroll
+
+    forEachChangedFinger(e, function(x,y, id) {
+        moveBox(id, x,y);
+    });
+})
+
+document.body.addEventListener('touchend', function(e){
 //    var touchObj = e.changedTouches[0] // reference first touch point for this eventx
 //
 //    console.log("POSITIONS: "+ "X : "+startScreenX+ ", Y: "+startScreenY+ ", Moved: "+movedDistance);
@@ -39,7 +46,8 @@ document.body.addEventListener('touchstart', function(e){
 //    console.log("Changed positions: X: " + startPageY + ", Now X: " + touchObj.pageY + ", Moved X: " + (touchObj.pageY-startPageY));
 //
 //    e.preventDefault()
-//}, false)
+
+})
 
 // Touch хийсэн газар хөлийн мөр харуулах
 function createFootPrint() {
@@ -61,6 +69,36 @@ function createFootPrint() {
     Android.vibrationStart();
 }
 
+function forEachChangedFinger(e, cb) {
+
+    for (var i = 0; i < e.changedTouches.length; i++) {
+        var finger = e.changedTouches[i];
+        var x = event.touches[i].pageX;
+        var y = event.touches[i].pageY;
+        var id = finger.identifier;
+        cb(finger, id);
+    }
+}
+
+function createDiv(finger,id) {
+    var div = document.createElement('div');
+    div.style.backgroundImage = "url('footprint.png')";
+    div.style.position = "absolute";
+    div.style.width = '52px';
+    div.style.height = '121px';
+    div.style.left = finger.pageX+'px';
+    div.style.top = finger.pageY+'px';
+    div.id(id);
+    document.body.appendChild(div);
+}
+function moveBox(id, x,y) {
+    var div = $("#"+id);
+    var off = $("body").offset();
+    // offset box a little so it can be seen under a finger!
+    var a = x - off.left - 35;
+    var b = y - off.top - 35;
+    div.css({"left":a, "top":b});
+}
 var checkScrollSpeed = (function(settings){
     settings = settings || {};
 
@@ -91,95 +129,45 @@ window.onscroll = function(){
     checkScrollSpeed()
     console.log( checkScrollSpeed() );
 };
-
-jQuery.scrollSpeed = function(step, speed, easing) {
-
-    var $document = $(document),
-        $window = $(window),
-        $body = $('html, body'),
-        option = easing || 'default',
-        root = 0,
-        scroll = false,
-        scrollY,
-        scrollX,
-        view;
-
-    if (window.navigator.msPointerEnabled)
-
-        return false;
-
-    $window.on('mousewheel DOMMouseScroll', function(e) {
-
-        var deltaY = e.originalEvent.wheelDeltaY,
-            detail = e.originalEvent.detail;
-            scrollY = $document.height() > $window.height();
-            scrollX = $document.width() > $window.width();
-            scroll = true;
-
-        if (scrollY) {
-
-            view = $window.height();
-
-            if (deltaY < 0 || detail > 0)
-
-                root = (root + view) >= $document.height() ? root : root += step;
-
-            if (deltaY > 0 || detail < 0)
-
-                root = root <= 0 ? 0 : root -= step;
-
-            $body.stop().animate({
-
-                scrollTop: root
-
-            }, speed, option, function() {
-
-                scroll = false;
-
-            });
-        }
-
-        if (scrollX) {
-
-            view = $window.width();
-
-            if (deltaY < 0 || detail > 0)
-
-                root = (root + view) >= $document.width() ? root : root += step;
-
-            if (deltaY > 0 || detail < 0)
-
-                root = root <= 0 ? 0 : root -= step;
-
-            $body.stop().animate({
-
-                scrollLeft: root
-
-            }, speed, option, function() {
-
-                scroll = false;
-
-            });
-        }
-
-        return false;
-
-    }).on('scroll', function() {
-
-        if (scrollY && !scroll) root = $window.scrollTop();
-        if (scrollX && !scroll) root = $window.scrollLeft();
-
-    }).on('resize', function() {
-
-        if (scrollY && !scroll) view = $window.height();
-        if (scrollX && !scroll) view = $window.width();
-
-    });
-};
-
-jQuery.easing.default = function (x,t,b,c,d) {
-
-    return -c * ((t=t/d-1)*t*t*t - 1) + b;
-};
-
-jQuery.scrollSpeed(10, 10);
+//
+//var lastScrollPosition = document.body.scrollTop,
+//    sectionNo = 0;
+//
+//function doScroll(scrollPosition, step, first) {
+//    var height = window.innerHeight,
+//        min = height * sectionNo,
+//        max = height * (sectionNo + 1);
+//
+//    scrollPosition += step;
+//
+//    if (min < scrollPosition && scrollPosition < max) {
+//        // here should be some animation control
+//        document.body.scrollTop = scrollPosition;
+//        // Call next animation frame
+//        window.requestAnimationFrame(doScroll.bind(null, scrollPosition, step));
+//    } else {
+//        // It fires, when scroll is done
+//        lastScrollPosition = scrollPosition;
+//        document.addEventListener("scroll", scrollListener);
+//    }
+//}
+//function scrollListener(e) {
+//    var scrollPosition = document.body.scrollTop,
+//        step;
+//
+//    // Stop scroll listening
+//    document.removeEventListener("scroll", scrollListener);
+//
+//    // Get direction
+//    step = scrollPosition >= lastScrollPosition ? 1 : -1;
+//    step = step * 2;
+//    // Go back to initial position
+//    document.body.scrollTop = lastScrollPosition;
+//
+//    // Animate
+//    window.requestAnimationFrame(doScroll.bind(null, lastScrollPosition, step, true));
+//}
+//
+//document.addEventListener("scroll", scrollListener);
+//
+//
